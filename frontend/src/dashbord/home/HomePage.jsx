56c7ef3dc25559605474b7ad46988f652c546716
SUHEL <sohelmbstucse@gmail.com>
2022-01-18 22:29:04 +0600

    add revenue page and add revenue and cost cart in home page

--- frontend/src/dashbord/home/HomePage.jsx
+++ frontend/src/dashbord/home/HomePage.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export  const HomePage=()=> {
     return (
         <div class="home-content">
+
         <div class="overview-boxes">
         
         <div class="box">
@@ -34,6 +35,46 @@ export  const HomePage=()=> {
         <div class="box">
           <div class="right-side">
             <div class="box-topic">Month's Transaction</div>
+            <div class="number">11,086</div>
+            
+          </div>
+          <i class='bx bxs-cart-download cart four' ></i>
+        
+        </div>
+      </div>
+
+
+      <div class="overview-boxes">
+        
+        <div class="box">
+          <div class="right-side">
+            <div class="box-topic">Total Cost</div>
+            <div class="number">40,876</div>
+            </div>
+          <i class='bx bx-cart-alt cart'></i>
+        </div>
+
+        <div class="box">
+          <div class="right-side">
+            <div class="box-topic">Total Revenue</div>
+            <div class="number">38,876</div>
+          
+          </div>
+          <i class='bx bxs-cart-add cart two' ></i>
+        </div>
+
+        <div class="box">
+          <div class="right-side">
+            <div class="box-topic">Today's Costs</div>
+            <div class="number">$12,876</div>
+            
+          </div>
+          <i class='bx bx-cart cart three' ></i>
+        </div>
+
+        <div class="box">
+          <div class="right-side">
+            <div class="box-topic">Todays's  Revenue</div>
             <div class="number">11,086</div>
             
           </div>
