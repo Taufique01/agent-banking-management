ec8bb750327681dc4ac8af75cd7a820d2178e6a9
Taufique01 <taufiquehr@gmail.com>
2022-01-25 23:12:09 +0600

    update requirements.txt

--- backend/settings.py
+++ backend/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-from dotenv import load_dotenv
 
-load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "hshd7dye2nb3bnbduyd6egh2cdwdgdcwsdudyu"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
